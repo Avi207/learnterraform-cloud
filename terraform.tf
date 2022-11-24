82053d29e33aa2804a65923ed38eb6b131ecae26
Avi207 <60266462+Avi207@users.noreply.github.com>
2022-11-24 13:04:01 +0530

    first commit

---
+++ terraform.tf
@@ -0,0 +1,17 @@
+terraform {
+  cloud {
+    organization = "demo-app-2022"
+
+    workspaces {
+      name = "learn-terraform-cloud"
+    }
+  }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.28.0"
+    }
+  }
+
+  required_version = ">= 0.14.0"
+}
