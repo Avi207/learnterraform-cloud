2e0d1d855585264a1209ed696095f30f054918e0
Avi207 <60266462+Avi207@users.noreply.github.com>
2022-11-24 16:25:08 +0530

    min change

--- terraform.tf
+++ terraform.tf
@@ -3,7 +3,7 @@ terraform {
     organization = "demo-avi-terraform"
 
     workspaces {
-      name = "learn-terraform-cloud"
+      name = "learnterraform-cloud"
     }
   }
   required_providers {
